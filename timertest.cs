54c31fcd1b5aeed3ee9be5295381c921424ebf47
buildcomplete <buildcomplete@users.noreply.github.com>
2019-09-07 15:35:43 +0200

    Create timertest.cs
    Initial test of timer (it has a bug)

---
+++ timertest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace timer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var t2000 = new Timer(2000) { AutoReset = false };
+			var waitForTimeout = new System.Threading.AutoResetEvent(false);
+			t2000.Elapsed += (s, e) => waitForTimeout.Set();
+
+			// stop 't2000' after one second, 
+			// and start it again, 
+			// we would expect our precision timer to run about 3 seconds now (1+2seconds)
+			var t1000 = new Timer(1000) { AutoReset = false };
+			t2000.Elapsed += (s, e) => { t2000.Stop(); t2000.Start(); };
+
+			var precisionTimer = Stopwatch.StartNew();
+			precisionTimer.Start();
+			t2000.Start();
+			t1000.Start();
+
+			waitForTimeout.WaitOne();
+			precisionTimer.Stop();
+
+			Console.WriteLine($"Timeout after {precisionTimer.ElapsedMilliseconds}ms");
+		}
+	}
+
+}
